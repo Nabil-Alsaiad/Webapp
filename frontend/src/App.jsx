0dbebd9a8f88b0108ac25cbb3f71981b8cef0efa
The Alpha <contact@nabilalsaiad.com>
2024-02-07 12:59:36 +0800

    Update main files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminSidebar from "./components/Admin/AdminSidebar";
 import DeveloperSidebar from "./components/Developer/DeveloperSidebar";
 import DeliverySidebar from "./components/Delivery/DeliverySidebar";
@@ -10,25 +9,24 @@ import SecuritySidebar from "./components/Security/SecuritySidebar";
 import VisitorSidebar from "./components/Visitor/VisitorSidebar";
 import TenantSidebar from "./components/Tenant/TenantSidebar";
 import Login from "./Login";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<AdminSidebar />} />
-          <Route path="/developer" element={<DeveloperSidebar />} />
-          <Route path="/delivery" element={<DeliverySidebar />} />
-          <Route path="/contractor" element={<ContractorSidebar />} />
-          <Route path="/agent" element={<AgentSidebar />} />
-          <Route path="/security" element={<SecuritySidebar />} />
-          <Route path="/visitor" element={<VisitorSidebar />} />
-          <Route path="/tenant" element={<TenantSidebar />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminSidebar />} />
+        <Route path="/developer" element={<DeveloperSidebar />} />
+        <Route path="/delivery" element={<DeliverySidebar />} />
+        <Route path="/contractor" element={<ContractorSidebar />} />
+        <Route path="/agent" element={<AgentSidebar />} />
+        <Route path="/security" element={<SecuritySidebar />} />
+        <Route path="/visitor" element={<VisitorSidebar />} />
+        <Route path="/tenant" element={<TenantSidebar />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

087c6bb20f58c0beeb18c2d818321f127c0d425c
The Alpha <contact@nabilalsaiad.com>
2024-02-10 19:17:50 +0800

    Update SQL files

--- sql/data.sql
+++ sql/data.sql
@@ -1,43 +1,54 @@
 --- Insert dummy data into the tables
+INSERT INTO
+  accounts (type_id, email, password, name, phone)
+VALUES
+  (
+    1,
+    'yes@test.pro',
+    '1234',
+    'Yes Bro',
+    '+60123456789'
+  );
 
-INSERT INTO accounts (type_id, email, password) VALUES
-(1, 'one@email.com', '601234567890'),
-(2, 'one@email.com', '601998877661'),
-(3, 'one@email.com', '601112233442'),
-(4, 'four@email.com', '601456789013'),
-(5, 'five@email.com', '601234567804');
+INSERT INTO
+  accounts (type_id, email, password)
+VALUES
+  (2, 'one@email.com', '1234'),
+  (4, 'four@email.com', '1234'),
+  (5, 'five@email.com', '1234');
 
-INSERT INTO visitors (name, phone) VALUES
-('Ahmad Ali', '601234567890'),
-('Norah Tan', '601998877661'),
-('Ravi Kumar', '601112233442'),
-('Siti Aishah', '601456789013'),
-('David Lim', '601234567804');
+INSERT INTO
+  accounts_extra (acc_id, license_id, vehicle_plate, company_name)
+VALUES
+  (1, '981122334455', '1234ABC', 'ABC Corporation'),
+  (2, '961234567891', '1234ABC', 'PQR Group'),
+  (3, '950987654321', '1234ABC', 'Tech Solutions');
 
-INSERT INTO deliveries (name, phone, email, license_id, company_id) VALUES
-('Ahmad Ali', '601234567890', 'one@gmail.com', '981122334455', 1),
-('Norah Tan', '601998877661', 'two@gmail.com', '990011223344', 2),
-('Ravi Kumar', '601112233442', 'three@gmail.com', '971234567890', 3),
-('Siti Aishah', '601456789013', 'four@gmail.com', '961234567891', 4),
-('David Lim', '601234567804', 'five@gmail.com', '950987654321', 5);
+INSERT INTO
+  visits (acc_id, exit_datetime, unit)
+VALUES
+  (1, '2024-01-13 10:00:00', 'A-07-01'),
+  (2, '2024-01-13 11:30:00', 'B-11-10'),
+  (3, '2024-01-13 12:45:00', 'C-09-08'),
+  (4, '2024-01-13 14:15:00', 'D-15-08'),
+  (5, '2024-01-13 15:30:00', 'E-14-02');
 
-INSERT INTO companies (name, hot_line) VALUES
-('ABC Corporation', '60123456788'),
-('XYZ Industries', '60199887676'),
-('LMN Ltd', '60111223341'),
-('PQR Group', '60145678900'),
-('Tech Solutions', '6012348888');
+CREATE TABLE
+  announcements (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );
 
-INSERT INTO vehicles (number, color, type) VALUES
-('ABC123', 'Red', 'Car'),
-('XYZ456', 'Blue', 'Motorcycle'),
-('LMN789', 'Green', 'Truck'),
-('PQR012', 'Black', 'SUV'),
-('TUV345', 'White', 'Van');
-
-INSERT INTO visits (visitor_id, exit_datetime, unit, vehicle_id) VALUES
-(1, '2024-01-13 10:00:00', 'A-07-01', 1),
-(2, '2024-01-13 11:30:00', 'B-11-10', NULL),
-(3, '2024-01-13 12:45:00', 'C-09-08', NULL),
-(4, '2024-01-13 14:15:00', 'D-15-08', 4),
-(5, '2024-01-13 15:30:00', 'E-14-02', NULL);
+INSERT INTO
+  announcements (title, description)
+VALUES
+  (
+    'Maintenance Work',
+    'We are going to perform maintenance work on 2nd January 2024. Please make sure to park your vehicle in the designated area.'
+  ),
+  (
+    'Security Alert',
+    'We have received reports of suspicious activities in the area. Please be cautious and report any suspicious activities to the security.'
+  );
--- sql/start.sql
+++ sql/start.sql
@@ -1,67 +1,67 @@
-CREATE DATABASE IF NOT EXISTS visitor_management
-USE visitor_management
+CREATE DATABASE IF NOT EXISTS visitor_management;
+
+USE visitor_management;
 
 DROP TABLE IF EXISTS acc_types;
-CREATE TABLE acc_types (
+
+CREATE TABLE
+  acc_types (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name CHAR(12) NOT NULL UNIQUE
-);
-INSERT INTO acc_types (name) VALUES
-('admin'),
-('agent'),
-('contractor'),
-('delivery'),
-('security'),
-('developer'),
-('tenant'),
-('visitor');
+    name CHAR(15) NOT NULL UNIQUE
+  );
+
+INSERT INTO
+  acc_types (name)
+VALUES
+  ('admin'),
+  ('agent'),
+  ('contractor'),
+  ('delivery'),
+  ('security'),
+  ('developer'),
+  ('tenant'),
+  ('visitor');
 
 DROP TABLE IF EXISTS accounts;
-CREATE TABLE accounts (
-    type_id INT NOT NULL REFERENCES acc_types(id),
+
+CREATE TABLE
+  accounts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type_id INT NOT NULL REFERENCES acc_types (id),
     email CHAR(50) NOT NULL,
     password CHAR(50) NOT NULL,
-    PRIMARY KEY (type_id, email)
-);
+    name VARCHAR(50),
+    phone CHAR(12),
+    register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
 
-DROP TABLE IF EXISTS visitors;
-CREATE TABLE visitors (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    phone CHAR(15) NOT NULL UNIQUE
-);
+DROP TABLE IF EXISTS accounts_extra;
 
-DROP TABLE IF EXISTS deliveries;
-CREATE TABLE deliveries (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    phone CHAR(15) NOT NULL UNIQUE,
-	email CHAR(50) NOT NULL UNIQUE,
-    license_id CHAR(30) NOT NULL UNIQUE,
-    company_id INT NOT NULL REFERENCES companies(id)
---    register_date DATETIME NOT NULL
-);
-
-DROP TABLE IF EXISTS companies;
-CREATE TABLE companies (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-    hot_line CHAR(15) NOT NULL UNIQUE
-);
-
-DROP TABLE IF EXISTS vehicles;
-CREATE TABLE vehicles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    number VARCHAR(10) NOT NULL NOT NULL,
-    color VARCHAR(15) NOT NULL,
-    type VARCHAR(30) NOT NULL
-);
+CREATE TABLE
+  accounts_extra (
+    acc_id INT NOT NULL PRIMARY KEY REFERENCES accounts (id),
+    license_id INT NOT NULL,
+    vehicle_plate CHAR(10) NOT NULL,
+    company_name VARCHAR(50) NOT NULL REFERENCES companies (id)
+  );
 
 DROP TABLE IF EXISTS visits;
-CREATE TABLE visits (
-    visitor_id INT NOT NULL PRIMARY KEY REFERENCES visitors(id),
+
+CREATE TABLE
+  visits (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    acc_id INT NOT NULL REFERENCES accounts (id),
     enter_datetime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    exit_datetime DATETIME NOT NULL,
-    unit CHAR(7) NOT NULL,
-    vehicle_id INT REFERENCES vehicles(id)
-);
+    exit_datetime DATETIME,
+    unit CHAR(7) NOT NULL
+  );
+
+DROP TABLE IF EXISTS announcements;
+
+CREATE TABLE
+  announcements (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );
